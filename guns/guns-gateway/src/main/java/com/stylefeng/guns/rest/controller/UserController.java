ef74a53fb5b7d18ba746b2fa96b2b609f854e00a
Jerio1997 <727627621@qq.com>
2019-11-28 21:41:25 +0800

    created by Jerio.

--- guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/controller/UserController.java
+++ guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/controller/UserController.java
@@ -3,6 +3,8 @@ package com.stylefeng.guns.rest.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.stylefeng.guns.api.user.UserService;
+import com.stylefeng.guns.rest.modular.vo.ResponseVO;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,19 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("user")
 public class UserController {
 
-
-
-
     @Reference
     private UserService userService;
 
     /*@RequestMapping("register")
     public ResponseVO*/
 
-    /*@GetMapping("logout")
-    public String logout(){
-
-    }*/
+    @GetMapping("logout")
+    public ResponseVO logout(){
+//        ResponseVO responseVO = new ResponseVO();
+        return null;
+    }
 
 
 }
