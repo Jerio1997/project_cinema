256d9e4913edcc1bcac4c914a7efc01fb23276d7
Jerio1997 <727627621@qq.com>
2019-11-28 22:33:12 +0800

    AuthController

--- guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/controller/AuthController.java
+++ guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/controller/AuthController.java
@@ -1,8 +1,14 @@
 package com.stylefeng.guns.rest.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Author Jerio
  * CreateTime 2019/11/28 22:23
  **/
+@RestController
 public class AuthController {
+
+    
 }
