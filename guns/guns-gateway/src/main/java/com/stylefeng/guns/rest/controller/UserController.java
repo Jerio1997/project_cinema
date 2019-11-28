19ba959571b3d629a353a25e0fc9a74b649a4785
xxxing007 <xxxing007@foxmail.com>
2019-11-28 21:07:38 +0800

    User服务

--- guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/controller/UserController.java
+++ guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/controller/UserController.java
@@ -1,10 +1,17 @@
 package com.stylefeng.guns.rest.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.stylefeng.guns.api.user.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
+
+    @Reference
+    private UserService userService;
+
+//    @RequestMapping("register")
 
 }
--- guns/guns-user/src/main/java/service/impl/UserServiceImpl.java
+++ guns/guns-user/src/main/java/service/impl/UserServiceImpl.java
@@ -1,7 +1,11 @@
 package service.impl;
 
-
+import com.alibaba.dubbo.config.annotation.Service;
 import com.stylefeng.guns.api.user.UserService;
+import org.springframework.stereotype.Component;
 
+@Component
+@Service
 public class UserServiceImpl implements UserService {
+
 }
