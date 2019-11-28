1db3f140a64d8436644a6594c63338d4c6397473
Jerio1997 <727627621@qq.com>
2019-11-28 21:09:40 +0800

    created by Jerio. controller

--- guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/controller/UserController.java
+++ guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/controller/UserController.java
@@ -1,10 +1,19 @@
 package com.stylefeng.guns.rest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
+
+
+
+    @GetMapping("logout")
+    public String logout(){
+        
+    }
 
 }
