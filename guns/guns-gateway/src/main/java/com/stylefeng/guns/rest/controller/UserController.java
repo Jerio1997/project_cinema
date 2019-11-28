c4fd563a7a8e6e8351ba7eb052384e280005cb1e
xxxing007 <xxxing007@foxmail.com>
2019-11-28 22:01:54 +0800

    User服务

--- guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/controller/UserController.java
+++ guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/controller/UserController.java
@@ -3,11 +3,9 @@ package com.stylefeng.guns.rest.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.stylefeng.guns.api.user.UserService;
+import com.stylefeng.guns.api.user.vo.User;
 import com.stylefeng.guns.rest.modular.vo.ResponseVO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
@@ -16,8 +14,19 @@ public class UserController {
     @Reference
     private UserService userService;
 
-    /*@RequestMapping("register")
-    public ResponseVO*/
+    @PostMapping("register")
+    public ResponseVO register(@RequestBody User user) {
+
+        User registerUser = userService.selectUserByUsernamePassword(user);
+        if (registerUser == null) {
+            boolean isRegister = userService.insertUserByUsernamePassword(user);
+            if (isRegister) {
+                return ResponseVO.success("注册成功");
+            }
+            return ResponseVO.appFail("系统出现异常，请联系管理员");
+        }
+        return ResponseVO.serviceFail("用户已存在");
+    }
 
     @GetMapping("logout")
     public ResponseVO logout(){
