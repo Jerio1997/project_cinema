c3bc4d1273b74cddb2a68f8f1c0105b37b1bf17b
xxxing007 <xxxing007@foxmail.com>
2019-11-29 16:33:13 +0800

    User服务

--- guns/guns-api/src/main/java/com/stylefeng/guns/api/user/UserService.java
+++ guns/guns-api/src/main/java/com/stylefeng/guns/api/user/UserService.java
@@ -1,8 +1,15 @@
 package com.stylefeng.guns.api.user;
 
 import com.stylefeng.guns.api.user.vo.User;
+import com.stylefeng.guns.api.user.vo.UserInfo;
 
 public interface UserService {
 
     boolean register(User user);
+
+    boolean checkUsername(String username);
+
+    boolean updateUserInfo(UserInfo userInfo);
+
+    UserInfo queryUserById(Integer uuid);
 }
