78a38e00f1c6c8cacf3a2e776c4df6f5a53b9bd3
Jerio1997 <727627621@qq.com>
2019-11-29 15:55:02 +0800

    created by Jerio . auth

--- guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/controller/UserController.java
+++ guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/controller/UserController.java
@@ -32,10 +32,10 @@ public class UserController {
 
     }
 
-    /*@GetMapping("logout")
+    @GetMapping("logout")
     public ResponseVO logout(){
-
-    }*/
+        return ResponseVO.success("用户退出成功");
+    }
 
 
     /*@GetMapping("getUserInfo")
