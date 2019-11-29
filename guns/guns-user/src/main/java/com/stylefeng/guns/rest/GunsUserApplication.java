2b92d4a6e99ee4eac96a19618489859e6c52e4bd
xxxing007 <xxxing007@foxmail.com>
2019-11-29 14:16:05 +0800

    User服务

--- guns/guns-user/src/main/java/com/stylefeng/guns/rest/GunsUserApplication.java
+++ guns/guns-user/src/main/java/com/stylefeng/guns/rest/GunsUserApplication.java
@@ -1,9 +1,11 @@
 package com.stylefeng.guns.rest;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.stylefeng.guns"})
+@MapperScan("com.stylefeng.guns.rest.common.persistence.dao")
 public class GunsUserApplication {
 
     public static void main(String[] args) {
