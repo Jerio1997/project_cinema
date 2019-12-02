85fbae0bb8b9722039268aa11ab5e86d2ad1768b
xxxing007 <xxxing007@foxmail.com>
2019-12-02 15:17:59 +0800

    order服务

---
+++ guns/guns-order/src/main/java/com/stylefeng/guns/order/common/persistence/dao/MoocOrderTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.order.common.persistence.dao;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.stylefeng.guns.order.common.persistence.model.MoocOrderT;
+
+/**
+ * <p>
+ * 订单信息表 Mapper 接口
+ * </p>
+ *
+ * @author xxxing
+ * @since 2019-12-02
+ */
+public interface MoocOrderTMapper extends BaseMapper<MoocOrderT> {
+
+}
