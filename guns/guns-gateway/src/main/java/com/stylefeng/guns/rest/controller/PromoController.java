fe696b5011682493cb289d80bc73f2e544538b02
Helios1102 <Helios1102@163.com>
2019-12-03 16:38:43 +0800

    测试branch

---
+++ guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/controller/PromoController.java
@@ -0,0 +1,11 @@
+package com.stylefeng.guns.rest.controller;
+
+/**
+ * @program: guns-parent
+ * @description: 秒杀功能的Controller
+ * @author: Helios1102
+ * @create: 2019-12-03 16:37
+ */
+public class PromoController {
+    
+}
